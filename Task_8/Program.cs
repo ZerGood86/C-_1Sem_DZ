c1f3c0c6a4d1d106d0fc7165efa46ce390582d01
ZerGood <miamoto007@gmail.com>
2022-08-12 21:44:48 +0500

    Rename Task 8

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿//Задача 8: Напишите программу, которая на вход принимает 
+//число (N), а на выходе показывает все чётные числа 
+//от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Enter N");
+int N = int.Parse(Console.ReadLine());
+
+if(N > 1)
+{
+    int x = N / 2;
+    int count = 0;
+    while(count < x)
+    {
+        int E = (count + 1) * 2;
+        Console.Write(E + " ");
+        count = count + 1;
+    }
+}
+else
+{
+   Console.WriteLine("\nError");
+}
