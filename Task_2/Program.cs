789118d175a9859be95df2194e32175c912c7eb6
ZerGood <miamoto007@gmail.com>
2022-08-12 14:33:22 +0500

    working Task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("\n"+a);
+}
+if(a == b)
+{
+    Console.WriteLine("\nЧисла равны");
+}
+if(a < b)
+{
+    Console.WriteLine("\n"+b);
+}
